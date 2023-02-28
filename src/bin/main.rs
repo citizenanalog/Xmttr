86fb817ea1cd00d3da0d7bb12b7d9229990facd0
Matt Sharpe <matt.sharpe@emerson.com>
2023-02-28 15:09:44 -0700

    Move ThreadPool to submod.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,21 +10,34 @@ extern crate xmttr;
 use xmttr::{
     connect_and_read::{get_com_port, mod_main},
     modbusmap::{self, build_hashmap},
-    ThreadPool,
+    pool::ThreadPool,
 };
 
 fn main() {
+    #[cfg(target_os = "windows")]
+    pub fn get_com_port() -> &'static str {
+        let com_list: &str = "COM9";
+        com_list
+    }
+    #[cfg(target_os = "linux")]
+    pub fn get_com_port() -> &'static str {
+        let com_list = ["/dev/ttyACM0", "/dev/ttyACM1"];
+        com_list
+    }
     let path = String::from("ModbusMap.csv");
     let my_hmap: HashMap<u16, String> = build_hashmap(&path);
 
     let pool = ThreadPool::new(2);
-    let com_list = ["/dev/ttyACM0", "/dev/ttyACM1"];
+    //let com_list = ["/dev/ttyACM0", "/dev/ttyACM1"];
+
+    let com_list = vec![get_com_port()];
     for device in com_list {
         let hmap = my_hmap.clone();
         pool.execute(move || {
-            mod_main(&hmap, device);
+            mod_main(&hmap, device.as_ref());
         });
     }
+    //main doesn't wait on running threads and ThreadPool is dropped,
 
-    println!("Shutting down.");
+    //println!("Shutting down.");
 }
